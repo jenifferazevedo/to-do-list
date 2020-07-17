795a276e9277d85a2eb8c5050c479a39fe582399
Jeniffer Azevedo <jeniqf@gmail.com>
2020-07-17 14:58:19 +0100

    Doing the year dinamic on footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  public year: Date;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.year = new Date();
   }
 
+  ngOnInit(): void {}
 }
